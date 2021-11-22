a932cef92f291e15b7b91c2dbfe226a6de91cc8d
Vasanth Kumar <vasanth@napses.com>
2021-11-22 14:15:56 +0530

    cleanup

--- public/admin.js
+++ public/admin.js
@@ -15,56 +15,56 @@ fetch('/getall')
   .then((data) => {
     console.log(data);
     allUsersData = data;
-    let tableChild= '';
-    let users = '';
+    let tableChild = '';
+    let users = '<option>choose</option>';
     data.forEach(item => {
-        users += `<option value=${item.id}>${item.name}</option>`        
-        select.innerHTML = users;
+      users += `<option value=${item.id}>${item.name}</option>`
+      select.innerHTML = users;
     })
   });
 
-  function dateFormatter(date) {
-    const dateObj = new Date(date);
-    const day = dateObj.toDateString().slice(0, 3);
-    const dateWithMonth = dateObj.toDateString().slice(4, 10).split(' ').reverse().join(' ')
-    return `${day}, ${dateWithMonth}`;
+function dateFormatter(date) {
+  const dateObj = new Date(date);
+  const day = dateObj.toDateString().slice(0, 3);
+  const dateWithMonth = dateObj.toDateString().slice(4, 10).split(' ').reverse().join(' ')
+  return `${day}, ${dateWithMonth}`;
 }
 
-  function dateTimeFormatter(date) {
-    const currentDate = new Date(date);
-    const hours = currentDate.getHours();
-    const minutes = currentDate.getMinutes();
-    const displayHours = hours > 12 ? hours - 12 : hours;
+function dateTimeFormatter(date) {
+  const currentDate = new Date(date);
+  const hours = currentDate.getHours();
+  const minutes = currentDate.getMinutes();
+  const displayHours = hours > 12 ? hours - 12 : hours;
 
-    return `${dateFormatter(date)} at ${[10, 11, 12].includes(displayHours) ? displayHours : ('0' + displayHours)}:${minutes} ${(hours >= 12 ? ' PM' : ' AM')}`;
+  return `${dateFormatter(date)} at ${[10, 11, 12].includes(displayHours) ? displayHours : ('0' + displayHours)}:${minutes} ${(hours >= 12 ? ' PM' : ' AM')}`;
 }
 
-  getUserDetails = () => {
-    let tableChild= '';
-    console.log(allUsersData, 'allUsersData')
-    console.log(select.value)
-    const res = allUsersData.find(item => item.id === +select.value);
-    console.log(res)
-    res.UserDetails.forEach((record, index) => {
-        tableChild += `<tr>
-            <th scope="row">${index+1}</th>
+getUserDetails = () => {
+  let tableChild = '';
+  console.log(allUsersData, 'allUsersData')
+  console.log(select.value)
+  const res = allUsersData.find(item => item.id === +select.value);
+  console.log(res)
+  res.UserDetails.forEach((record, index) => {
+    tableChild += `<tr>
+            <th scope="row">${index + 1}</th>
             <td>${res.name}</td>
             <td>${record.weight}</td>
             <td>${record.bmi.toFixed(2)}</td>
             <td>${dateTimeFormatter(record.createdAt)}</td>
             <td>${record.message}</td>
-          </tr>` 
-    })        
-        tableBody.innerHTML = tableChild;
-        const sortedData = res.UserDetails.sort((a,b) => a > b)
-        const allTimeHighBmiText = `bmi : ${sortedData[0].bmi}`
-        const allTimeHighWeightText = `weight : ${sortedData[0].weight}`
-        const allTimeLowWeightText = `weight : ${sortedData[sortedData.length -1].weight}`
-        const allTimeLowBmiText = `bmi : ${sortedData[sortedData.length -1].bmi}`;
-        allTimeHighWeight.innerHTML = allTimeHighWeightText;
-        allTimeLowWeight.innerHTML  =allTimeLowWeightText;
-        allTimeHighBmi.innerHTML = allTimeHighBmiText;
-        allTimeLowBmi.innerHTML = allTimeLowBmiText;
-    }
-  
-  select.addEventListener('change', () => getUserDetails());
+          </tr>`
+  })
+  tableBody.innerHTML = tableChild;
+  const sortedData = res.UserDetails.sort((a, b) => a > b)
+  const allTimeHighBmiText = `bmi : ${sortedData[0].bmi}`
+  const allTimeHighWeightText = `weight : ${sortedData[0].weight}`
+  const allTimeLowWeightText = `weight : ${sortedData[sortedData.length - 1].weight}`
+  const allTimeLowBmiText = `bmi : ${sortedData[sortedData.length - 1].bmi}`;
+  allTimeHighWeight.innerHTML = allTimeHighWeightText;
+  allTimeLowWeight.innerHTML = allTimeLowWeightText;
+  allTimeHighBmi.innerHTML = allTimeHighBmiText;
+  allTimeLowBmi.innerHTML = allTimeLowBmiText;
+}
+
+select.addEventListener('change', () => getUserDetails());
