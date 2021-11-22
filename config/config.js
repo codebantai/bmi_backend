6235e6d86d89550f8259cae7b99bdec09ad5a251
Vasanth Kumar <vasanth@napses.com>
2021-11-22 14:05:41 +0530

    added production credential

--- config/config.js
+++ config/config.js
@@ -1,10 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    // url: process.env.DATABASE_URL,
-    // dialect: 'postgres',
-
-    // port: process.env.PORT || 5000,
     "development": {
         username: "root",
         password: "root",
